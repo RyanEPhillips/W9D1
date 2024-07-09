e6ce1ec9e01dd8d61d0db659329bd80ff5191730
2085Anony420 <85RyanP@gmail.com>
2024-07-08 20:31:53 -0400

    update code inputs with assignments

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,10 +1,13 @@
-array = [10, 23, 45, 70, 11, 15]
-def bubbleSort(arr):
+array1 = [64, 34, 25, 12, 22, 11, 90]
+array2 = [5, 1, 4, 2, 8]
+def bubble_sort(arr):
     n = len(arr)
     for i in range(n):
         for j in range(0, n-i-1):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
 
-bubbleSort(array)
-print(array) 
+
+print(f"Sorted array: {bubble_sort(array1)}")
+print(f"Sorted array: {bubble_sort(array2)}") 
